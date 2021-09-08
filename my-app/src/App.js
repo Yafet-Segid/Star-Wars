5d7cc143922d89eb356fa8f014cf18beb52beddc
Yafet Segid <yafetsegid@gmail.com>
2021-09-08 05:40:17 -0500

    clean code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,57 +8,38 @@ export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-     
       starList: [],
     };
     this.getPages = this.getPages.bind(this);
   }
   async componentDidMount() {
-   this.fetchCharacters(1);
+    this.fetchCharacters(1);
   }
-  fetchCharacters = async (page) =>{
-    const characters = await fetch(`https://swapi.dev/api/people/?page=${page}`);
-    const response = await characters.json();
+  fetchCharacters = async (page) => {
+    const response = await fetch(`https://swapi.dev/api/people/?page=${page}`);
+    const characters = (await response.json()).results;
 
-    // species
+    for (const character of characters) {
+      const homeworld = character.homeworld;
+      const response = await fetch(homeworld);
+      character.homeworld = (await response.json()).name;
 
-    for (const race of response.results) {
-      if (race.species.length === 0) {
-        race.species = "Human";
+      // for (character.species.length === 0) {
+      if (character.species.length === 0) {
+        character.species = "Human";
       } else {
-        const species = race.species;
-        const specieResponse = await fetch(species).then((resp) => resp.json());
-        race.species = specieResponse.name;
+        const speciesURL = character.species[0];
+        const response = await fetch(speciesURL);
+        const speciesName = (await response.json()).name;
+        character.species = speciesName;
       }
     }
 
-    // homeworld
-    for (const character of response.results) {
-      const homeworld = character.homeworld;
-      const homeworldResponse = await fetch(homeworld).then((res) =>
-        res.json()
-      );
-      character.homeworld = homeworldResponse.name;
+    this.setState({ starList: characters });
+  };
 
-      this.setState({ starList: response.results });
-    }
-  }
-
-  // Change page
-  // getPages = (event) => {
-  //   const { data, pageSize } = this.state;
-  //   this.setState({
-  //     currentPage: event,
-  //     dataToShow: data.slice(
-  //       pageSize * (event - 1),
-  //       pageSize * (event - 1) + 9
-  //     ),
-  //   });
-  // };
-
-  //  OR
   getPages(page) {
-   this.fetchCharacters(page);
+    this.fetchCharacters(page);
   }
 
   render() {
